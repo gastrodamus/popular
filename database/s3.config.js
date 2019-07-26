e91c1c4eda945b8f2ac7009da8a0cc58fc9a8d84
Minho Choo <mchoo7@gatech.edu>
2019-07-26 13:58:08 -0700

    add aws s3 config file and dependencies(aws, aws-sdk, request), setup s3 image seeding file

---
+++ database/s3.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  AWS_ACCESS_KEY: 'AKIAIQVGUH7GVZMAVGLA',
+  AWS_SECRET_ACCESS_KEY: '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',
+  REGION: 'us-west-1',
+  bucket: 'gastrodamus-images',
+};
---
+++ database/s3Upload.js
@@ -0,0 +1,41 @@
+const AWS = require('aws-sdk');
+const request = require('request');
+const env = require('./s3.config');
+
+AWS.config.update({
+  accessKeyId: env.AWS_ACCESS_KEY,
+  secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+  region: env.REGION,
+});
+
+const s3 = new AWS.S3();
+
+const uploadImage = (url, fileName, callback) => {
+  request({ url: url, encoding: null }, (err, res, body) => {
+    if (err) {
+      console.log('fail to request the image');
+    } else {
+      const param = {
+        ACL: 'public-read',
+        Bucket: 'gastrodamus-images',
+        Body: body,
+        Key: fileName,
+      };
+      s3.putObject(param, (error, data) => {
+        if (error) {
+          console.log('fail to upload the image');
+          callback(err);
+        }
+      });
+    }
+  });
+};
+
+for (let i = 0; i < 1000; i += 1) {
+  const image = 'https://source.unsplash.com/600x400/?dish';
+  uploadImage(image, `dish/${i + 1}.png`, (err, data) => {
+    if (err) {
+      console.log('error occured: ', err);
+    }
+  });
+}
