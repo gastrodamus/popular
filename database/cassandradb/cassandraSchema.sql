988ed45093eaf4fa3706ed15a6480c2fd17d99f7
Minho Choo <mchoo7@gatech.edu>
2019-07-31 13:19:58 -0700

    made cassandra schema

--- database/cassandradb/cassandraSchema.sql
+++ database/cassandradb/cassandraSchema.sql
@@ -1,22 +1,28 @@
-CREATE TABLE restaurant (
-   restaurant_id   int,
-   restaurant_name varchar,
-   PRIMARY KEY(restaurant_id)
+DROP KEYSPACE IF EXISTS restaurants_popular_dishes;
+CREATE KEYSPACE IF NOT EXISTS restaurants_popular_dishes
+WITH replication = {
+   'class': 'SimpleStrategy',
+   'replication_fator': 1
+};
+
+CREATE TABLE restaurants (
+   restaurant_id  uuid  PRIMARY KEY,
+   restaurant_name      text
 );
+
 /* photo_count and review_count will be replaced with foreign key in the proxy version */
-CREATE TABLE dish (
-   dish_id       int,
-   restaurant_id int,
+CREATE TABLE dishes_by_restaurants (
+   restaurant_id uuid,
+   dish_id       int, --colume
    dish_images   varchar,
    dish_name     varchar,
    price_dish,   int,
    photo_count,  int,
+   review_count  int,
    PRIMARY KEY (restaurant_id, dish_id)
 );
 
-CREATE TABLE review (
-   review_id   int,
-   restaurant_id int,
-   dish_id varchar,
-   PRIMARY KEY(review_id, restaurant_id, dish_id)
-);
+
+--keyspace -> restaurants_popular_dishes
+--PRIMARY KEY(<partition key>,<cluster key>)
+--
